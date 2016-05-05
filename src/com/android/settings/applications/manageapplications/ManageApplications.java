164ccded9dd28726876d61ee912da14b391bfec5
Roman Royer <romanroyer@yahoo.com>
2016-05-04 19:04:36 -0700

    Settings: Allow sorting applications by size
    Change-Id: I9e022162110e1feff20f257992086e66735e0f5e
    
    Conflicts:
    	src/com/android/settings/applications/manageapplications/ManageApplications.java

--- src/com/android/settings/applications/manageapplications/ManageApplications.java
+++ src/com/android/settings/applications/manageapplications/ManageApplications.java
@@ -631,10 +631,10 @@ public class ManageApplications extends InstrumentedFragment
         }
         mOptionsMenu.findItem(R.id.advanced).setVisible(false);
 
-        mOptionsMenu.findItem(R.id.sort_order_alpha).setVisible( 
+        mOptionsMenu.findItem(R.id.sort_order_alpha).setVisible(
                 (mListType == LIST_TYPE_STORAGE || mListType == LIST_TYPE_MAIN)
                 && mSortOrder != R.id.sort_order_alpha);
-        mOptionsMenu.findItem(R.id.sort_order_size).setVisible( 
+        mOptionsMenu.findItem(R.id.sort_order_size).setVisible(
                 (mListType == LIST_TYPE_STORAGE || mListType == LIST_TYPE_MAIN)
                 && mSortOrder != R.id.sort_order_size);
 
