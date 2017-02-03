e8abfece5777059acb4437b4a751cd342b597333
ezio84 <brabus84@gmail.com>
2017-02-03 16:40:55 +0100

    Allow to quickly open Running Services from QS panel [2/2]
    Change-Id: I8f1bc70d75aa2f07031f30a2fd408e220b8574e6
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,5 +177,5 @@ public class Settings extends SettingsActivity {
     public static class SkyDragonSettingsActivity extends SettingsActivity {}
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ButtonSettingsActivity extends SettingsActivity { /* empty */ }
-
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
