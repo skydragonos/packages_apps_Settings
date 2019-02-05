nitrogen-project/android_packages_apps_Settings

Initial checkin on Pie for SmartNav navigation suite [2/4]
Interesting changes for bring up:

The DUI project is now called SmartNav in a effort to bring
broader generalization to the project

The SmartNav settings have been moved to a separate project.
This will allow for MUCH EASIER adaptation for projects in which
only minimal touching of Settings and project specific settings
is required.

Elimination of the previously required utils jar shared library!
We moved all the utils classes to fwb. This eliminated the need
to hax up bp files and touch makefiles all over to use utils
methods, as well as simplifies project integration.

Wrapping stock navigation implementation in the Navigator
abstraction is becoming increasingly difficult as more
"things" interact with stock navigation buttons. We handle this
situation with button null checks as well as "isStockNav()" checks.

As SystemUI continues to compartmentalize, required notification media
methods in StatusBar moved to a separate class. Adaptation for Pulse
required the introduction of a new callback from NotificationMediaManager

Android.mk
@@ -20,6 +20,9 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../NitrogenSettings/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartNavSettings/src)
+
+LOCAL_FULL_LIBS_MANIFEST_FILES += $(LOCAL_PATH)/AndroidManifest-SmartNav.xml
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
@@ -51,7 +54,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/NitrogenSettings/res
+    packages/apps/NitrogenSettings/res \
+    packages/apps/SmartNavSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 

skydragonos/packages_apps_Settings

Android.mk
@@ -20,6 +20,9 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../SkyDragonSettings/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartNavSettings/src)
+
+LOCAL_FULL_LIBS_MANIFEST_FILES += $(LOCAL_PATH)/AndroidManifest-SmartNav.xml
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-slices-builders \
@@ -52,7 +55,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/SkyDragonSettings/res
+    packages/apps/SkyDragonSettings/res \
+    packages/apps/SmartNavSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 